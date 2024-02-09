760e41ae52f6fffc196b11c6b100569b83e73c13
manleeeemon <manleeeemon@gmail.com>
2024-02-09 15:02:39 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import WeatherComponent from "./WeatherComponent";
+import styles from './index.css';
+
+function App() {
+  const [city, setCity] = useState("130000");
+
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  return (
+    <>
+      <h1>日本天気予報アプリ</h1>
+      <InputForm setCity={setCity} handleChange={handleChange} />
+      <WeatherComponent cityCode={city} />
+    </>
+  );
+}
+
+function InputForm({ setCity, handleChange }) {
+  return (
+
+    <form>
+      <select onChange={handleChange}>
+        <option value="130000">東京</option>
+        <option value="270000">大阪</option>
+        <option value="016000">札幌</option>
+      </select>
+
+    </form>
+  );
+}
+
+export default App;
